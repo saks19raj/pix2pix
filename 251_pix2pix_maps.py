ea5fe42cbce2d05a737dceef15ed092f6ccbc8b4
saks19raj <117379895+saks19raj@users.noreply.github.com>
2023-01-07 00:01:53 +0530

    Add files via upload

---
+++ 251_pix2pix_maps.py
@@ -0,0 +1,143 @@
+# https://youtu.be/6pUSZgPJ3Yg
+"""
+Satellite image to maps image translation ​using Pix2Pix GAN
+ 
+Data from: http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/maps.tar.gz
+Also find other datasets here: http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/
+"""
+
+from os import listdir
+from numpy import asarray, load
+from numpy import vstack
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import load_img
+from numpy import savez_compressed
+from matplotlib import pyplot
+import numpy as np
+
+# load all images in a directory into memory
+def load_images(path, size=(256,512)):
+	src_list, tar_list = list(), list()
+	# enumerate filenames in directory, assume all are images
+	for filename in listdir(path):
+		# load and resize the image
+		pixels = load_img(path + filename, target_size=size)
+		# convert to numpy array
+		pixels = img_to_array(pixels)
+		# split into satellite and map
+		sat_img, map_img = pixels[:, :256], pixels[:, 256:]
+		src_list.append(sat_img)
+		tar_list.append(map_img)
+	return [asarray(src_list), asarray(tar_list)]
+
+# dataset path
+path = 'maps/train/'
+# load dataset
+[src_images, tar_images] = load_images(path)
+print('Loaded: ', src_images.shape, tar_images.shape)
+
+
+n_samples = 3
+for i in range(n_samples):
+	pyplot.subplot(2, n_samples, 1 + i)
+	pyplot.axis('off')
+	pyplot.imshow(src_images[i].astype('uint8'))
+# plot target image
+for i in range(n_samples):
+	pyplot.subplot(2, n_samples, 1 + n_samples + i)
+	pyplot.axis('off')
+	pyplot.imshow(tar_images[i].astype('uint8'))
+pyplot.show()
+
+#######################################
+
+from pix2pix_model import define_discriminator, define_generator, define_gan, train
+# define input shape based on the loaded dataset
+image_shape = src_images.shape[1:]
+# define the models
+d_model = define_discriminator(image_shape)
+g_model = define_generator(image_shape)
+# define the composite model
+gan_model = define_gan(g_model, d_model, image_shape)
+
+#Define data
+# load and prepare training images
+data = [src_images, tar_images]
+
+def preprocess_data(data):
+	# load compressed arrays
+	# unpack arrays
+	X1, X2 = data[0], data[1]
+	# scale from [0,255] to [-1,1]
+	X1 = (X1 - 127.5) / 127.5
+	X2 = (X2 - 127.5) / 127.5
+	return [X1, X2]
+
+dataset = preprocess_data(data)
+
+from datetime import datetime 
+start1 = datetime.now() 
+
+train(d_model, g_model, gan_model, dataset, n_epochs=10, n_batch=1) 
+#Reports parameters for each batch (total 1096) for each epoch.
+#For 10 epochs we should see 10960
+
+stop1 = datetime.now()
+#Execution time of the model 
+execution_time = stop1-start1
+print("Execution time is: ", execution_time)
+
+#Reports parameters for each batch (total 1096) for each epoch.
+#For 10 epochs we should see 10960
+
+#################################################
+
+#Test trained model on a few images...
+from tensorflow.keras.models import save_model
+save_model(g_model, "model.h5")
+from keras.models import load_model
+from numpy.random import randint
+model = load_model('model.h5')
+
+# plot source, generated and target images
+def plot_images(src_img, gen_img):
+	images = vstack((src_img, gen_img))
+	# scale from [-1,1] to [0,1]
+	images = (images + 1) / 2.0
+	titles = ['Source', 'Generated']
+	# plot images row by row
+	for i in range(len(images)):
+		# define subplot
+		pyplot.subplot(1, 3, 1 + i)
+		# turn off axis
+		pyplot.axis('off')
+		# plot raw pixel data
+		pyplot.imshow(images[i])
+		# show title
+		pyplot.title(titles[i])
+	pyplot.show()
+
+
+#Giving own image 
+
+import cv2
+SIZE_X = 256
+SIZE_Y = 256
+test_src_img =cv2.imread("C:\\Users\\saksh\\Downloads\\italy.jpg", 1)
+test_src_img = cv2.resize(test_src_img, (SIZE_Y, SIZE_X), interpolation = cv2.INTER_NEAREST)
+test_src_img = (test_src_img - 127.5) / 127.5
+test_src_img = np.expand_dims(test_src_img, axis=0)
+
+gen_test_image = g_model.predict(test_src_img)
+plot_images(test_src_img, gen_test_image)
+pyplot.imshow(gen_test_image[0, :,:,0], cmap='gray')
+optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+loss, accuracy = model.evaluate(X1, X2, verbose=1)
+
+print("Test: accuracy = %f  ;  loss = %f" % (accuracy, loss))
+
+
+
+
